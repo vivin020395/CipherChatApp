3430eb3caca66a3f90170207bb8575ccfd96508c
VIVIN <vivin02031995@gmail.com>
2017-07-03 15:46:06 +0530

    Added offline capabilities for account settings

---
+++ app/src/main/java/com/example/vivin/chatapp/CipherChat.java
@@ -0,0 +1,25 @@
+package com.example.vivin.chatapp;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.OkHttpDownloader;
+import com.squareup.picasso.Picasso;
+
+
+public class CipherChat extends Application {
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+        Picasso.Builder builder = new Picasso.Builder(this);
+        builder.downloader(new OkHttpDownloader(this,Integer.MAX_VALUE));
+        Picasso built = builder.build();
+        built.setLoggingEnabled(true);
+        Picasso.setSingletonInstance(built);
+
+    }
+}
